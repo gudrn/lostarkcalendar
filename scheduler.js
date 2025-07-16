8e257e6a489f2e2ef5eebe4d0d9b96f80c965b2c
김형구 <113834617+gudrn@users.noreply.github.com>
2025-07-16 11:53:29 +0900

    메세지 수정정

--- scheduler.js
+++ scheduler.js
@@ -10,12 +10,12 @@ export const scheduleIslandAlerts = (client) => {
       const message = await getTodayGoldIslands();
       if (!message) {
         await channel.send(`오늘은 골드섬이 없습니다.`);
-        await channel.send(`오늘도 즐거운 로생되세요.`);
+        await channel.send(`즐거운 로생되세요.`);
         await channel.send(`----------------------------------------`);
         return;
       }
       await channel.send(`@everyone\n⏰ 오늘 골드를 주는 모험섬\n${message} `);
-      await channel.send(`오늘도 즐거운 로생되세요.`);
+      await channel.send(`즐거운 로생되세요.`);
       await channel.send(`----------------------------------------`);
     } catch (err) {
       console.error("❌ 자동 알림 전송 실패:", err);
